15418c8a24f742cbb884b3b30f1f301b5f7dd334
dhanraj <dhanarajyshetty12@gmail.com>
2023-09-02 11:18:02 +0530

    1

---
+++ python/py.py
@@ -0,0 +1,15 @@
+def add (n1,n2):
+    return(n1+n2)
+
+def mul(n1,n2):
+    return(n1*n2)
+
+def div(n1,n2):
+    return(n1/n2)
+
+
+a=int(input("Enter num1 :"))
+b=int(input("Enter num2 :"))
+print("Ans=",add(a,b))
+print("Ans=",mul(a,b))
+print("Ans=",div(a,b))
