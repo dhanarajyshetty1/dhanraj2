d1582ed5fa4fc028b4917c95893c0434ee01d30a
dhanraj <dhanarajyshetty12@gmail.com>
2023-09-02 11:26:58 +0530

    subtraction added

--- python/py.py
+++ python/py.py
@@ -4,6 +4,9 @@ def add (n1,n2):
 def mul(n1,n2):
     return(n1*n2)
 
+def sub(n1,n2):
+    return(n1-n2)
+
 def div(n1,n2):
     return(n1/n2)
 
@@ -11,5 +14,6 @@ def div(n1,n2):
 a=int(input("Enter num1 :"))
 b=int(input("Enter num2 :"))
 print("Ans=",add(a,b))
+print("Ans=",sub(a,b))
 print("Ans=",mul(a,b))
 print("Ans=",div(a,b))
